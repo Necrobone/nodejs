7ff0b580218c80ee9cf9234ea6b2882e6dd650eb
Necrobone <necrobone@hotmail.com>
2022-03-15 20:33:08 +0100

    Basic routing request

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    console.log(request.url, request.method, request.headers);
+    const url = request.url;
+
+    if (url === '/') {
+        response.write('<html>');
+        response.write('<head><title>Enter Message</title></head>');
+        response.write(
+            '<body>' +
+                '<form action="/message" method="POST">' +
+                    '<input type="text" name="message"/>' +
+                    '<button type="submit">Send</button>' +
+                '</form>' +
+            '</body>'
+        );
+        response.write('</html>');
+
+        return response.end();
+    }
 
     response.setHeader('Content-Type', 'text/html');
 
