47b7e3a83f4009c2c8025a9deb43603779862675
Necrobone <necrobone@hotmail.com>
2022-03-15 20:27:55 +0100

    Basic response

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    console.log(request);
+    console.log(request.url, request.method, request.headers);
+
+    response.setHeader('Content-Type', 'text/html');
+
+    response.write('<html>');
+    response.write('<head><title>My First Page</title></head>');
+    response.write('<body><h1>Hello from my Node.js Server</h1></body>');
+    response.write('</html>');
+
+    response.end();
 });
 
 server.listen(3000);
