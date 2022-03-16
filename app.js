a20f03e7936e1d49a7d3de5aa0670910437aeb15
Necrobone <necrobone@hotmail.com>
2022-03-16 20:16:05 +0100

    Splitting routes and exporting

--- app.js
+++ app.js
@@ -1,57 +1,7 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    const method = request.method;
+const routes = require('./routes');
 
-    if (url === '/') {
-        response.write('<html>');
-        response.write('<head><title>Enter Message</title></head>');
-        response.write(
-            '<body>' +
-                '<form action="/message" method="POST">' +
-                    '<input type="text" name="message"/>' +
-                    '<button type="submit">Send</button>' +
-                '</form>' +
-            '</body>'
-        );
-        response.write('</html>');
-
-        return response.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-
-        request.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.text', message);
-        })
-
-        response.writeHead(302, {
-            location: '/',
-        });
-
-        return response.end();
-    }
-
-    response.setHeader('Content-Type', 'text/html');
-
-    response.write('<html>');
-    response.write('<head><title>My First Page</title></head>');
-    response.write('<body><h1>Hello from my Node.js Server</h1></body>');
-    response.write('</html>');
-
-    response.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
