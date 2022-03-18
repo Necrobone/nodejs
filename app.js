6bcf2b34cbded1ca14f1d142e7907c9afadd2f4c
Necrobone <necrobone@hotmail.com>
2022-03-18 20:20:30 +0100

    Basic routing and parsing

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const express = require('express');
+
 const app = express();
 
-app.use('/users', (request, response, next) => {
-    console.log('/users middleware');
-    request.send('<p>The Middleware that handles just /users</p>');
+app.use(express.urlencoded({
+    extended: false,
+}));
+
+app.get('/add-product', (request, response) => {
+    console.log('/add-product middleware');
+    response.send(
+        '<form action="/product" method="post">' +
+            '<input type="text" name="title" />' +
+            '<button type="submit">Add Product</button>' +
+        '</form>'
+    );
 });
 
-app.use('/', (request, response, next) => {
+app.post('/product', (request, response) => {
+    console.log(request.body);
+    response.redirect('/');
+});
+
+app.get('/', (request, response) => {
     console.log('/ middleware');
-    request.send('<p>The Middleware that handles just /</p>');
+    response.send('<p>The Middleware that handles just /</p>');
 });
 
 app.listen(3000);
