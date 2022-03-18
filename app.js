fc2f0bb2c03002b2d53ab9076af882272b990197
Necrobone <necrobone@hotmail.com>
2022-03-18 19:39:04 +0100

    Middleware testing

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 
-const server = http.createServer(app);
+app.use((request, response, next) => {
+    console.log('In the middleware!');
+    next();
+});
 
-server.listen(3000);
+app.use((request, response, next) => {
+    console.log('In another middleware!');
+    response.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
