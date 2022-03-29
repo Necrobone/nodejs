2d7b9b7f7ebdc908bd407a8e57a82b42342cf865
Necrobone <necrobone@hotmail.com>
2022-03-29 20:15:59 +0200

    Installing MySQL

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
 app.use(express.urlencoded({
     extended: false,
