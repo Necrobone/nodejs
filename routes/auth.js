5ed87239790be67006115b68685cd5a4a5775a68
Necrobone <necrobone@hotmail.com>
2022-05-24 21:21:16 +0200

    Password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator/check');
+const { body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -8,7 +8,20 @@ const Router = express.Router();
 Router.get('/login', authController.getLogin);
 Router.get('/signup', authController.getSignup);
 Router.post('/login', authController.postLogin);
-Router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+Router.post(
+    '/signup',
+    [
+        body('email', 'Please enter a valid email').isEmail(),
+        body('password', 'Please enter a password with number, text and 5 characters').isLength({min: 5}).isAlphanumeric(),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Password confirmation does not match password');
+            }
+
+            return true;
+        }),
+    ],
+    authController.postSignup);
 Router.post('/logout', authController.postLogout);
 Router.get('/reset', authController.getReset);
 Router.post('/reset', authController.postReset);
